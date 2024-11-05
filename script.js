69a3ac62830e073d047525095c32dab4aa5ad885
Siddhesh Ghag <147908529+Slayer-X164@users.noreply.github.com>
2024-11-05 15:52:53 +0530

    search filter done

--- script.js
+++ script.js
@@ -1,31 +1,53 @@
-const productsContainer = document.querySelector('.products');
-const searchInput = document.querySelector('.search')
-const catContainer = document.querySelector('.catContainer')
-const priceRange = document.querySelector('.priceRange')
-const PriceValue = document.querySelector('.PriceValue')
+const productsContainer = document.querySelector(".products");
+const searchInput = document.querySelector(".search");
+const catContainer = document.querySelector(".catContainer");
+const priceRange = document.querySelector(".priceRange");
+const PriceValue = document.querySelector(".PriceValue");
+
+let productData = [];
 
 async function getProductData() {
-    const response = await fetch('https://fakestoreapi.com/products');
-    const data = await response.json();
+  const response = await fetch("https://fakestoreapi.com/products");
+  const data = await response.json();
+  productData = data;
+  console.log(productData);
 
-    displayProduct(data);
+  displayProduct(productData);
 }
-getProductData();
 
-const displayProduct = (filteredProduct)=>{
-    productsContainer.innerHTML+=filteredProduct.map((eachProduct)=>{
+const displayProduct = (filteredProduct) => {
+  productsContainer.innerHTML = "";
+
+  if (filteredProduct.length === 0) {
+    productsContainer.innerHTML = `<p>NO Products Found</p>`;
+  } else {
+    productsContainer.innerHTML = filteredProduct
+      .map((eachProduct) => {
         return `
-        <div class="product">
-            <img src="${eachProduct.image}" alt="">
-            <span class="title">${eachProduct.title}</span>
-            <span class="priceText">$${eachProduct.price}</span>
-        </div>
+            <div class="product">
+                <img src="${eachProduct.image}" alt="">
+                <span class="title">${eachProduct.title}</span>
+                <span class="priceText">$${eachProduct.price}</span>
+            </div>
 
-        `
-    }).join("")
-}
+            `;
+      })
+      .join("");
+  }
+};
 
-searchInput.addEventListener('keyup',(e)=>{
-    console.log(e.target.value);
+searchInput.addEventListener("input", () => {
+  const searchTerm = searchInput.value.toLowerCase();
+  const filteredData = productData.filter((product) => {
+    return (
+      product.title.toLowerCase().includes(searchTerm) ||
+      product.description.toLowerCase().includes(searchTerm)
+    );
+  });
 
-})
+  console.log(filteredData);
+
+  displayProduct(filteredData);
+});
+
+getProductData();
